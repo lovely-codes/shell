77ee7116476583ec3396831934ab2c99c53f68d6
xingyue <qixingyue@126.com>
2016-01-19 16:33:14 +0800

    简单的vim bundle 控制linux版本

---
+++ basic_vim_bundle_linux.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cp /etc/vimrc /etc/vimrc.bk
+git clone https://github.com/gmarik/vundle.git /usr/share/vim/vimfiles/bundle/vundle
+
+cat >>/etc/vimrc <<EOF
+"-------------------start custom config for golang------------------------"
+set nu
+filetype indent on
+set shiftwidth=4
+set softtabstop=4
+set tabstop=4
+
+" for vundle
+set rtp+=/usr/share/vim/vimfiles/bundle/vundle/  
+call vundle#rc('/usr/share/vim/vimfiles/bundle/')  
+Plugin 'gmarik/vundle'
+Plugin 'gmarik/vundle'
+Plugin 'neocomplcache'
+Plugin 'The-NERD-Commenter'
+Plugin 'bling/vim-airline'
+call vundle#end()
+
+EOF
+
+
+sleep 3
+
